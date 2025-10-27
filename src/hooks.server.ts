3894a0bc6b3a99fd6c054dc57fbd885bd3312d10
Max <m-freytag@gmx.de>
2025-10-27 12:12:52 +0100

    fix redirect redundancy

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,19 +1,6 @@
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-  const host = event.request.headers.get('host');
-  const url = new URL(event.request.url);
-  
-  // Redirect www to non-www
-  if (host?.startsWith('www.')) {
-    return new Response(null, {
-      status: 301,
-      headers: {
-        Location: `https://makegradients.app${url.pathname}${url.search}`
-      }
-    });
-  }
-  
   // Continue with normal request handling
   return await resolve(event);
 };
